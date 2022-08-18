7c7f3b5a2428e73aabc4f8e91b2b00caeefdf1b3
tomoron <tomoron326@outlook.jp>
2022-08-18 18:24:53 +0900

    add:assetPrefix

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+const prefixPath = isProd ? "/tomoron" : "";
+
 const nextConfig = {
+  assetPrefix: prefixPath,
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
