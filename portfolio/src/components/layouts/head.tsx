d99c66c0f31a98e84ec51b61997f047816fcfed8
tomoron <tomoron326@outlook.jp>
2022-08-19 20:54:06 +0900

    fix:baseurl

--- portfolio/src/components/layouts/head.tsx
+++ portfolio/src/components/layouts/head.tsx
@@ -1,10 +1,9 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 
 const MainHead = () => {
   return (
     <Head>
-      <title>Tomoshi%aposs portfolio web site</title>
+      <title>Tomoshi's portfolio</title>
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       <meta name='description' content='Tomoshi%aposs portfolio Homepage' />
       <link rel='icon' href='/favicon.ico' />
