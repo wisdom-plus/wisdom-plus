4b47790e0366786e3cc7bf467e14988c8cd467bf
tomoron <tomoron326@outlook.jp>
2022-08-19 20:42:28 +0900

    add:Head

---
+++ portfolio/src/components/layouts/head.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const MainHead = () => {
+  return (
+    <Head>
+      <title>Tomoshi%aposs portfolio web site</title>
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta name='description' content='Tomoshi%aposs portfolio Homepage' />
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+  )
+}
+
+export default MainHead
