f4f76426b875fdc30eab369693e05e6abc4a35f0
tomoron <tomoron326@outlook.jp>
2022-08-20 19:53:24 +0900

    add:style class

--- portfolio/src/components/layouts/head.tsx
+++ portfolio/src/components/layouts/head.tsx
@@ -1,13 +1,22 @@
 import Head from 'next/head'
 
-const MainHead = () => {
+type Props = {
+  children?: JSX.Element
+}
+
+const MainHead = ({ children }: Props) => {
   return (
-    <Head>
-      <title>Tomoshi's portfolio</title>
-      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-      <meta name='description' content='Tomoshi%aposs portfolio Homepage' />
-      <link rel='icon' href='/favicon.ico' />
-    </Head>
+    <div className='max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0'>
+      <Head>
+        <title>Tomoshi's portfolio</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta name='description' content='Tomoshi%aposs portfolio Homepage' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main className='w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0 '>
+        {children}
+      </main>
+    </div>
   )
 }
 
