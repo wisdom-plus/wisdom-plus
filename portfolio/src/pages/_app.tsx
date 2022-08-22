08bf3e6577723f4336f06cdb325b4b1064b3191a
tomoron <tomoron326@outlook.jp>
2022-08-22 23:54:43 +0900

    add:Image

--- portfolio/src/pages/_app.tsx
+++ portfolio/src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
+import BackImage from 'public/backimage.jpg'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Image src={BackImage} layout='fill' objectFit='cover' objectPosition='center' />
+    </>
+  )
 }
 
 export default MyApp
