cee4538ad55e2964734d0617cbc2275926232228
tomoron <tomoron326@outlook.jp>
2022-08-23 00:19:03 +0900

    add:next-export-optimize-images

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-const prefixPath = isProd ? "/tomoron" : "";
+const isProd = process.env.NODE_ENV === 'production'
+const prefixPath = isProd ? '/tomoron' : ''
+const withExportImages = require('next-export-optimize-images')
 
-const nextConfig = {
+const nextConfig = withExportImages({
   assetPrefix: prefixPath,
   reactStrictMode: true,
   swcMinify: true,
-};
+})
 
-module.exports = nextConfig;
+module.exports = nextConfig
