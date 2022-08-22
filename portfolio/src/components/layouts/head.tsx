d35c0ab4bb762b68e745348fc6fd49b0610738f1
tomoron <tomoron326@outlook.jp>
2022-08-22 23:55:11 +0900

    fix: tailwindcss

--- portfolio/src/components/layouts/head.tsx
+++ portfolio/src/components/layouts/head.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const MainHead = ({ children }: Props) => {
   return (
-    <div className='max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0'>
+    <div className='max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0 relative z-10'>
       <Head>
         <title>Tomoshi's portfolio</title>
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
@@ -18,9 +18,9 @@ const MainHead = ({ children }: Props) => {
       <main className='w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0 '>
         {children}
       </main>
-      <div className='w-full lg:w-2/5'>
+      <div className='w-full lg:w-2/5 hidden lg:block'>
         <Image
-          className='rounded-none lg:rounded-lg shadow-2xl lg:block hidden'
+          className='rounded-none lg:rounded-lg shadow-2xl'
           src={ProfileImage}
           objectFit='cover'
           priority
--- portfolio/src/pages/_document.tsx
+++ portfolio/src/pages/_document.tsx
@@ -1,12 +1,11 @@
 import Document, { Html, Main, Head, NextScript } from 'next/document'
-import Image from 'next/image'
 
 class Mydocument extends Document {
   render() {
     return (
       <Html lang='ja'>
         <Head />
-        <body className='font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover'>
+        <body className='font-sans antialiased text-gray-900 leading-normal tracking-wider'>
           <Main />
           <NextScript />
         </body>
